e5d5532c551683f08cb156bfaecc3a4596cf3a16
mrrevo55 <kwolny31@gmail.com>
2022-07-16 15:08:05 +0200

    Create user entity

---
+++ src/main/java/com/teamsfinder/userreadservice/user/model/Role.java
@@ -0,0 +1,8 @@
+package com.teamsfinder.userreadservice.user.model;
+
+enum Role {
+    USER,
+    MENTOR,
+    JURY,
+    ADMIN
+}
---
+++ src/main/java/com/teamsfinder/userreadservice/user/model/Tag.java
@@ -0,0 +1,17 @@
+package com.teamsfinder.userreadservice.user.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
