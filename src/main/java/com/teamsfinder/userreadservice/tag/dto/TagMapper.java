a6a832995533ac08512a98473ef91668762cd889
mrrevo55 <kwolny31@gmail.com>
2022-07-17 12:51:17 +0200

    Implement UserMapper.java and TagMapper.java

--- src/main/java/com/teamsfinder/userreadservice/tag/dto/TagMapper.java
+++ src/main/java/com/teamsfinder/userreadservice/tag/dto/TagMapper.java
@@ -1,16 +1,13 @@
 package com.teamsfinder.userreadservice.tag.dto;
 
 import com.teamsfinder.userreadservice.tag.model.Tag;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.experimental.UtilityClass;
 
-public abstract class TagMapper {
-    static TagDto mapToDto(Tag tag){
+@UtilityClass
+public class TagMapper {
+    public static TagDto mapToDto(Tag tag){
         return new TagDto(tag.getId(), tag.getName());
-    }
-
-    static Tag mapFromDto(TagDto tagDto){
-        return Tag.builder()
-                .id(tagDto.id())
-                .name(tagDto.name())
-                .build();
     }
 }
