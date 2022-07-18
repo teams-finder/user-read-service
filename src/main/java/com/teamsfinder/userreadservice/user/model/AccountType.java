4abd0e0525a1edc9d85a8980b16a7260817a4891
MrRevo <101434678+misterrevo@users.noreply.github.com>
2022-07-18 19:54:44 +0200

    Configuring user read service (#1)
    * initial commit
    
    * Added .gitignore
    
    * Delete target directory
    
    * Delete user-read-service.iml
    
    * Changed application.yml
    
    * Update .gitignore
    
    * Create user entity
    
    * Renamed user role to account_type
    
    * Removed @Data annotation from User.java
    
    * Moved Tag.java to tag.model package
    
    * Removed @Data annotation in Tag.java
    
    * Add not null to keycloakid and account type
    
    * Removed unused @NoArgsConstructor annotation om User.java

---
+++ src/main/java/com/teamsfinder/userreadservice/user/model/AccountType.java
@@ -0,0 +1,8 @@
+package com.teamsfinder.userreadservice.user.model;
+
+enum AccountType {
+    USER,
+    MENTOR,
+    JURY,
+    ADMIN
+}
---
+++ src/test/java/com/teamsfinder/userreadservice/UserReadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamsfinder.userreadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserReadServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
